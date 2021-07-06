ce05d80ce2f978849a06e5e54305744704f1579c
AleenaLuk <alef999teena@gmail.com>
2021-07-06 11:35:09 -0400

    comleted

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@
 
 // The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in a single output value.
 
+// # 1
 const pricesArray = items.map(function(item){
   return item.price;
 });
@@ -17,13 +18,47 @@ const totalPriceOfAllItems = pricesArray.reduce(function(acc, i) {
 });
 
 const averagePrice = totalPriceOfAllItems / items.length;
-console.log((averagePrice.toFixed(2)));
+console.log(`The average price is $${averagePrice.toFixed(2)}`);
+console.log(`\n`);
 
 
 
+
+// # 2
 
 const itemsWithinRange = items.filter(function(item) {
   return item.price >= 14 && item.price <= 18 && item.currency_code === "USD";
 });
 
-console.log(itemsWithinRange);
+console.log(`Items that cost between $14.00 USD and $18.00 USD:`);
+itemsWithinRange.forEach(item => console.log(item.title));
+console.log(`\n`);
+
+// # 3
+
+const gbpItems = items.filter(function(item) {
+  return item.currency_code === "GBP";
+});
+
+gbpItems.forEach(item => console.log(`${item.title} costs £${item.price}`))
+console.log('\n');
+// # 4
+
+const woodItems = items.filter(function(item) {
+  return item.materials.includes('wood')
+});
+
+woodItems.forEach(function(item) {
+  console.log(item.title);
+});
+
+const eightOrMoreMaterals = items.filter(function(item) {
+   return item.materials.length >= 8;
+});
+console.log(eightOrMoreMaterals)
+
+
+const itemsMadeBySeller = items.filter(function(item) {
+  return item.who_made === 'i_did';
+});
+console.log(itemsMadeBySeller)
